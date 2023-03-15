7a11fac640d65e5f821ecc041b54be337a51d614
Gonzalo Pompei <gonzalopompei@gmail.com>
2023-03-15 01:19:03 -0300

    2 commit rama1: Se crea funcion de menu y eleccion de servicio con diferentes funciones a probar

--- index.js
+++ index.js
@@ -28,5 +28,46 @@ const carrito = []
 
 console.log(stock)
 
+//----------Funciones--------------------//
+function menu(){
+    let salirMenu = false
+    do{
+        salirMenu = elegirServicio (salirMenu)
+    }while(!salirMenu)
+}
 
+function elegirServicio(salir){
+    let opcionIngresada = parseInt(prompt(`Bienvenido
+    1 - Ver productos disponibles
+    2 - Buscar Productos
+    3 - Agregar Producto
+    4 - Comprar
+    5 - Salir del Menu`))
+
+    switch(opcionIngresada){
+        case 1:
+            productosDisponibles(stock)
+        break;
+        case 2: 
+            filtrar()
+        break;
+        case 3:
+            agregarProducto()
+        break;
+        case 4:
+            comprar()
+        break;
+        case 5:
+            alert("Gracias por su visita")
+            salir= true
+            return salir
+        break;
+        default:
+            alert("Ingresa una opcion de la lista")
+        break;
+    }
+}
+
+
+menu()
 
